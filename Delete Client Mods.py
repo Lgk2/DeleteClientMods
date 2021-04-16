891f3592be8b2b166b9c54aaf09fce2d63faf269
Lgk2 <dunngen3@live.se>
2021-04-17 00:21:18 +0200

    Add files via upload

--- Delete Client Mods.py
+++ Delete Client Mods.py
@@ -1,21 +1,12 @@
 import os
 import glob
 
-client_mods = ["AmbientSounds", "BetterAdvancements", "BetterFoliage", "BetterFps", "BetterPlacement", "Blur",
-               "ChatCopier", "ChatFlow", "classicbar", "clearwater", "ClientTweaks", "ColouredTooltips", "Controlling",
-               "CTM-MC", "CustomBackgrounds", "CustomLoadingScreen", "CustomMainMenu", "CustomSkinLoader_Forge",
-               "DynamicSurroundings", "DynamicSurroundingsHuds", "dynamistics", "EntityCulling", "fluidict", "FpsReducer",
-               "FullscreenWindowed", "gendustryjei", "hiddenarmor", "ikwid", "InGameAccountSwitcher", "InGameInfoXML",
-               "InventoryHud", "InvMove", "InvMove", "itlt", "jeibees", "jeiintegration", "jeivillagers", "jepb",
-               "JEROreIntegration", "jetif", "just-enough-brewcraft", "just-enough-harvestcraft", "JustEnoughCharacters",
-               "JustEnoughEnergistics", "JustEnoughReactors", "JustEnoughResources", "lesslag", "LoadingScreens",
-               "lootcapacitortooltips", "MAGE", "MainMenuScale", "memorycleaner", "MenuMobs", "MineMenu", "MoBends",
-               "modnametooltip", "Modpack Configuration Checker", "MouseTweaks", "NonUpdate", "OldJavaWarning",
-               "PackModeMenu", "particleculling", "potiondescriptions", "ProportionalDestructionParticles",
-               "RealFirstPerson2", "ResourceLoader", "scalingguis", "ScreenshotToClipboard", "shapeselector",
-               "ShoulderSurfing", "shutupmodelloader", "SmoothFont", "soundsystemreloader", "SplashAnimation",
-               "StuffASockInIt", "tc6aspects4jei", "ThaumicJEI", "TieredTooltips", "tinkersjei", "Tips", "Toast Control",
-               "Unifine", "WHAT", "WorldSelectionAdvanced"]
+
+client_mods = []
+with open('mods.txt') as file:
+    for line in file:
+        for word in line.split():
+            client_mods.append(word)
 
 count = 0
 
@@ -29,4 +20,5 @@ for cmodIndex in range(len(client_mods)):
         count += 1
     else:
         print("Did not find " + cmodString)
+print("Tried to delete " + str(len(client_mods)) + " mods")
 print("Finished deleting client mods. Deleted " + str(count) + " mods!")
